7c29667d585791ade7a3930b44c819dcd8c20b04
Serhiy Vinichuk <serhiyvinichuk@gmail.com>
2014-11-19 11:30:28 +0200

    update product edit form

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -1,20 +1,13 @@
 <?php
 
 use common\models\Category;
-use kartik\widgets\Select2;
+use wbraganca\fancytree\FancytreeWidget;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Product */
 /* @var $form yii\widgets\ActiveForm */
-$categories = \yii\helpers\ArrayHelper::map(Category::find()->all(), 'id', 'name', function (Category $object, $defaultValue) {
-    if ($object instanceof Category) {
-        $parent = $object->getParentCategory()->one();
-        return empty($parent) ? $defaultValue : $parent->name;
-    }
-    return $defaultValue;
-});
 ?>
 
 <div class="product-form">
@@ -23,16 +16,15 @@ $categories = \yii\helpers\ArrayHelper::map(Category::find()->all(), 'id', 'name
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
 
-    <?= $form->field($model, 'categories')->widget(Select2::className(), [
-        'data' => $categories,
+    <?= $form->field($model, 'categories')->widget(FancytreeWidget::className(), [
         'options' => [
-            'placeholder' => 'Select a category ...',
-            'multiple' => true,
-        ],
-        'pluginOptions' => [
-            'allowClear' => true,
-        ],
-    ]); ?>
+            'source' => Category::find()->dataFancytree(),
+            'selectMode' => 2,
+            'checkbox' => true,
+            'titlesTabbable' => true,
+            'clickFolderMode' => 3,
+        ]
+    ]) ?>
 
     <?= $form->field($model, 'price')->textInput() ?>
 
@@ -43,7 +35,8 @@ $categories = \yii\helpers\ArrayHelper::map(Category::find()->all(), 'id', 'name
     <?= $form->field($model, 'file')->fileInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update',
+            ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
